833f8aa3ffe6bfddcd39e7bc6550e2bf828aa5d5
Efrain Diaz <eudp_95@hotmail.com>
2018-06-06 11:19:35 -0400

    routes for user registration

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,53 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var express = require('express');
+var userRoutes = express.Router();
+var Joi  = require('joi');
+var User = require('../models/User');
+
+// Validation schema for Joi
+
+const userSchema = Joi.object.keys({
+	email: Joi.string().email().required(),
+	userName: Joi.string().required(),
+	password: Joi.string().regex(/^[a-zA-z0-9]{6,30}$/).required(),
+	confirmationPassword: Joi.any().valid(Joi.ref('password')).required()
 });
 
-module.exports = router;
+userRoutes.get('/register')
+	.get((req,res) => {
+		res.render('register');
+	})
+	.post(async (req,res,next) => {
+		try {
+			const result = Joi.validate(req.body, userSchema);
+
+			if (result.error) {
+				req.flash('error', 'Data entered is not valid. Please try again.');
+				res.redirect('/users/register');
+				return;
+			}
+
+			const user = await User.findOne({'email' : result.value.email });
+			if (user) {
+				req.flash('error', 'Email is already in use');
+				res.redirect('/users/register');
+				return;
+			}
+
+			const hash = await User.hashPassword(result.value.password);
+
+			delete result.value.confirmationPassword;
+			result.value.password = hash;
+
+			const newUser = await new User(result.value);
+			await newUser.save();
+
+			req.flash('success', 'Registration sucessfully, go ahead and login.');
+			res.redirect('/users/login');
+		} catch(error) {
+			next(error);
+		}
+	}
+);
+module.exports = userRoutes;
