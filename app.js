897d6f6e0e580c33f0a6e5c07204e0e1cba17d12
Efrain Diaz <eudp_95@hotmail.com>
2018-06-14 10:58:03 -0400

    update app.js

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var session = require('express-session');
-var flash = require('connect-flash');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 
-var config = require('./Config');
+var mongoConfig = require('./mongoConfig');
 
 var indexRouter = require('./routes/index');
 var userRouter = require('./routes/users');
@@ -18,30 +13,16 @@ var todoRouter = require('./routes/todo');
 
 var app = express();
 
-//Connect to database
-mongoose.connect(config.DB);
+mongoose.connect(mongoConfig.DB);
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(session({
-	cookie: { maxAge: 60000},
-	secret: 'keyboard cat',
-	saveUninitialized: false,
-	resave: false
-}))
 
-app.use(flash());
-
-require('./passport/init')(passport, LocalStrategy);
-app.use(passport.initialize());
-app.use(passport.session());
+require('./passport')(app);
 
 app.use('/', indexRouter);
 app.use('/', userRouter(passport));
