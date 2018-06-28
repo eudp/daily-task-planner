3e86456d03ba084bac4c338070b83608ff57297c
Efrain Diaz <eudp_95@hotmail.com>
2018-06-28 08:55:44 -0400

    missing parentheses

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const htmlRouter = express.Router;
+const htmlRouter = express.Router();
 
 htmlRouter.use(express.static("client/build"));
 
