fba5f67fcdf9f6f0d33f91957f41c0ce87561e9b
Efrain Diaz <eudp_95@hotmail.com>
2018-06-30 20:35:13 -0400

    comments for provider pattern

--- client/src/services/withUser.js
+++ client/src/services/withUser.js
@@ -3,6 +3,10 @@ import React from 'react';
 const stateFromStore = sessionStorage.getItem('user');
 let state = stateFromStore ? JSON.parse(stateFromStore) : null;
 
+/**
+* this stores forceUpdate() functions for all mounted components
+* that need user state
+*/
 const subscribers = [];
 
 const unsubscribe = subscriber => {
@@ -12,6 +16,11 @@ const unsubscribe = subscriber => {
 
 const subscribe = subscriber => {
 	subscribers.push(subscriber);
+
+	/** 
+	* for convinience, subscribe returns a function to remove 
+	* the rerendering when it is no longer needed
+	*/
 	return () => unsubscribe(subscriber);
 };
 
@@ -27,6 +36,11 @@ export const withUser = Component => {
 		}
 
 		componentWillUnmount() {
+
+			/**
+			* remove rerenderComponent function since this component 
+			* don't need to be rerendered any more
+			*/
 			this.unsubscribe();
 		}
 	};
@@ -36,5 +50,10 @@ export const update = newState => {
 	state = newState;
 
 	sessionStorage.setItem('user', state ? JSON.stringify(state) : null);
+
+	/**
+	* rerender all wrapped components to reflect current user state
+	* rerendering allows it to use its new props or state
+	*/
 	subscribers.forEach(subscriber => subscriber());
 }
