54237c50ac3f469d89c48bcb4eab40fd7b21505c
Efrain Diaz <eudp_95@hotmail.com>
2018-07-09 12:30:09 -0400

    some breathing when importing

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -4,6 +4,7 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 import { Link } from 'react-router-dom';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+
 import { update } from '../services/withUser';
 
 class LoginPage extends Component {
