23efb04cf2f130902a05d0fe9820096813749ee0
Efrain Diaz <eudp_95@hotmail.com>
2018-07-09 12:31:08 -0400

    update to homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,69 +1,36 @@
-import axios from 'axios';
 import React, { Component, Fragment } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
 import { withUser } from '../services/withUser';
 
+import TodoList from '../components/TodoList';
+
 class HomePage extends Component {
-	state = {
-		tasks: null
-	}
-
-	componentDidMount() {
-		if (!this.props.user) {
-			return;
-		}
-
-		axios.get('/api/task')
-			.then(res => {
-				this.setState({
-					tasks: res.data
-				});
-			})
-			.catch(err => {
-				console.log(err);
-				this.setState({
-					tasks: []
-				});
-			});
-	}
-
+	
 	render() {
 
 		const { user } =  this.props;
-		const { tasks } = this.state;
+		
 		return(
-			<Fragment>
-				{user && tasks &&
-					<div>
-						Welcome back, {user.userName}!
 
-						{
-							tasks.map((currentValue, index) => 
-								
-								<div key={index}>
-									<h2>{currentValue.date}</h2>
-									{currentValue.tasks.length ? (
-										<ul>
-											{currentValue.tasks.map((currentValue, index) =>
-												<li key={index}>{currentValue.text}</li>
-											)}
-										</ul>
-									) : (
-										<p>Without tasks yet.</p>
-									)}
-							
-								</div>
-							)
-						}
-						
-					</div>
-				}
-				{user && !tasks &&
-					<div>Hold, on looking for your tasks...</div>
+			<Grid container>
+				{user &&
+
+					<Grid item xs={12}>
+						<TodoList/>
+					</Grid>
+
 				}
 				{!user &&
-					<div>Hey! I don't recognize you! Register and log in using the link above</div>
+					<Grid item xs={12}>
+						<Typography component="p">
+							Hey! I don't recognize you! Register and log in using the link above
+						</Typography>
+					</Grid>
 				}
-			</Fragment>
+			</Grid>
+	
 		);
 	}
 }
