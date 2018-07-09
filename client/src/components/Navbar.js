561141a0d44c5dcd3724a9b4b7bab9d654edd11e
Efrain Diaz <eudp_95@hotmail.com>
2018-07-09 12:31:52 -0400

    proper styles navbar and added user log in

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,28 @@
 import axios from 'axios';
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
+import { update } from '../services/withUser';
+
 import LoginButton from './LoginButton';
 import LoginMenu from './LoginMenu';
 
-import { update } from '../services/withUser';
+const styles = {
+	root: {
+		flexGrow: 1,
+		marginBottom:'10px'
+	},
+	flex: {
+		flex: 1
+	}
+};
 
 const Navbar = (props) => {
-	const { user } = props;
+	const { user, classes } = props;
 	const userName = user ? user.userName : null;
 
 	const handleLogIn = () => {
@@ -30,17 +41,20 @@ const Navbar = (props) => {
 	}
 
 	return (
-		<AppBar position="static">
-			<Toolbar>
-				<Typography variant="title" color="inherit">
-					To-Do
-				</Typography>
-				{user ?
-					<LoginMenu userName={userName} onLogOut={handleLogOut} />
-					: <LoginButton onClick={handleLogIn} />}
-			</Toolbar>
-		</AppBar>
+
+		<div className={classes.root}>
+			<AppBar position="static">
+				<Toolbar>
+					<Typography variant="title" color="inherit" className={classes.flex}>
+						Daily Task Planner {user ? `| ${user.userName}` : ``}
+					</Typography>
+					{user ?
+						<LoginMenu onLogOut={handleLogOut} />
+						: <LoginButton onClick={handleLogIn} />}
+				</Toolbar>
+			</AppBar>
+		</div>
 	);
 };
 
-export default withRouter(Navbar);
+export default withRouter(withStyles(styles)(Navbar));
