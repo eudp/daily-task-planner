220fec91abe1dcb235edce152e64a2a9b1bffca9
Efrain Diaz <eudp_95@hotmail.com>
2018-07-10 09:41:17 -0400

    adding padding with styles in add form

--- client/src/components/TodoAdd.js
+++ client/src/components/TodoAdd.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  paddingAddForm: {
+    paddingRight: '10% !important',
+    paddingLeft: '10% !important'
+  }
+}
 
 
 const TodoAdd = (props) => (
-  <Grid container spacing={24}>
-    <Grid item xs={1}/>
 
-    <Grid item xs={10}>
+  <Grid container spacing={24}>
+ 
+    <Grid item xs={12} className={props.classes.paddingAddForm} alignItems="center">
       <TextField
         name="todo-add"
         label="What needs to be done?"
@@ -17,9 +25,8 @@ const TodoAdd = (props) => (
       />
     </Grid>
 
-    <Grid item xs={1}/>
   </Grid>
 
 );
 
-export default TodoAdd;
+export default withStyles(styles)(TodoAdd);
