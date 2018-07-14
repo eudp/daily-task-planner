84f69ce9d66fdf2048677dca984d209d70a05e58
Efrain Diaz <eudp_95@hotmail.com>
2018-07-14 17:19:16 -0400

    update response error

--- app.js
+++ app.js
@@ -31,7 +31,9 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+  	message: err.message
+  });
 });
 
 module.exports = app;
